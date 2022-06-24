9060c8dfe3420d6b7ac068ae05dbc041f04f84ab
Divine <104062206+Divine-James@users.noreply.github.com>
2022-06-24 08:59:47 +0100

    added function

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-const num1 = parseInt(prompt('enter first number: '));
-const num2 = parseInt(prompt('enter second number: '));
-const operator = (prompt('enter operator: '));
-//const task = (prompt('would you like to perform another task?'));
+function calculator(){
+    const num1 = parseInt(prompt('enter first number: '));
+    const operator = (prompt('enter operator: +, -, *, / '));
+    const num2 = parseInt(prompt('enter second number: ')); 
+        if (operator ==="+"){
+            alert( num1+num2)
+           }else if(operator ==="-"){
+               alert(num1-num2)
+           }else if (operator ==="/"){
+               alert(num1/num2)
+           }else if(operator ==="*"){
+               alert(num1*num2)
+           }else{
+               alert('invalid operator: use +, -, / or *')
+           }
 
-    if (operator ==="+"){
-        alert(num1+num2)
-       }else if(operator ==="-"){
-           alert(num1-num2)
-       }else if (operator ==="/"){
-           alert(num1/num2)
-       }else if(operator ==="*"){
-           alert(num1*num2)
-       }else{
-           alert('invalid operator: use +, -, / or *')
-       }
+}
+calculator()
+const task = (prompt('would you like to perform another task? yes/no'));
+if (task === "yes"){
+    calculator()
+}else if (task === "no"){
+    alert('thanks for using calculator')
+}else{
+    alert('unrecongised input, enter either yes/no')
+}
+
+
+
 
